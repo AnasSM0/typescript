90ec2120211e233b1f97f9c4d91ab43fcb573697
AnasSM0 <anasbutt20067@gmail.com>
2024-02-19 00:03:01 +0500

    initial commit

---
+++ Your Own Array/main.js
@@ -0,0 +1,4 @@
+var cars = ["Honda NSX", "Mclaren P1", "Dodge Challenger", "Ferrari SF90", "Ford GT"];
+cars.forEach(function (car) {
+    console.log("I would love to own a ".concat(car, " one day."));
+});
---
+++ Your Own Array/main.ts
@@ -0,0 +1,8 @@
+let cars=["Honda NSX", "Mclaren P1", "Dodge Challenger","Ferrari SF90","Ford GT"];
+
+
+
+cars.forEach((car) => {
+
+    console.log(`I would love to own a ${car} one day.`); });
+
