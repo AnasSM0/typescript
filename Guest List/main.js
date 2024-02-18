78cf635c6350c87c1841ed7bcd3b6e4980567df6
AnasSM0 <anasbutt20067@gmail.com>
2024-02-19 00:09:02 +0500

    initial commit

---
+++ Guest List/main.js
@@ -0,0 +1,4 @@
+var Guests = ["Khalid Kashmiri", "Rustin Cohle", "Mike Tyson"];
+Guests.forEach(function (guest) {
+    console.log("I would love to invite Mr. ".concat(guest, " to a delicious dinner."));
+});
