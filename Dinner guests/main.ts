7ccf40385bf6a0c5e12a11801ab223ef9e9fdc23
AnasSM0 <anasbutt20067@gmail.com>
2024-03-02 16:26:28 +0500

    initial commit

---
+++ Dinner guests/main.ts
@@ -0,0 +1,9 @@
+let Guests =["Khalid Kashmiri","Rustin Cohle","Mike Tyson"];
+
+console.log(`\nSadly, Mr ${Guests[1]} can't make it to the dinner.`);
+
+Guests[1]="Bruce Wayne";
+
+console.log("\nUpdated Guests List: \n");
+
+console.log(`Now ${Guests.length} Guests will be joining us for dinner.`);
