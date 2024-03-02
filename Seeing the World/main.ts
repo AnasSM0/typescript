c71fd38843e4bd7ed15606c0617164333c3824f6
AnasSM0 <anasbutt20067@gmail.com>
2024-03-02 16:13:48 +0500

    initial commit

---
+++ Seeing the World/main.ts
@@ -0,0 +1,25 @@
+let Places=["Italy","Switzerland","Paris","Makkah","Madina"];
+
+console.log(Places); 
+
+console.log("\nPlaces in Alphabetical order: ");
+console.log([...Places].sort());
+
+console.log("\nOriginal Order: ");
+console.log(Places);
+
+console.log("\nReverse Alphabetical Order:");
+console.log([...Places].sort().reverse());
+
+console.log("\nOriginal Order:");
+console.log(Places);
+
+Places.reverse();
+console.log("\nReversed Order:");
+console.log(Places);
+
+console.log("\nSorted in Alphabetical Order after new order:");
+console.log(Places.sort());
+
+console.log("\nSorted in Reverse Alphabetical Order:");
+console.log(Places.sort().reverse());
