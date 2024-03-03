b8253a1a7f6a09bb4129bd801fef40e316823a1c
AnasSM0 <anasbutt20067@gmail.com>
2024-03-03 18:18:29 +0500

    initial commit

---
+++ Alien Colors #3/main.js
@@ -0,0 +1,10 @@
+var alien_color = "Green";
+if (alien_color == "Green") {
+    console.log("Congratulations! You have earned 5 points.");
+}
+else if (alien_color == "Yellow") {
+    console.log("Congratulations! You have earned 10 points.");
+}
+else if (alien_color == "Red") {
+    console.log("Congratulations! You have earned 15 points.");
+}
