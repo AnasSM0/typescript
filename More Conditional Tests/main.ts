a7c8629e1d2105a61db07d455e5ece6a66180ae0
AnasSM0 <anasbutt20067@gmail.com>
2024-03-03 16:52:55 +0500

    initial commit

---
+++ More Conditional Tests/main.ts
@@ -0,0 +1,42 @@
+let car ="Mclaren P1";
+
+console.log("Is the car Mclaren P1? I predict true");
+console.log(car=='Mclaren P1');
+
+console.log("\nIs the car Pagani Huayra? I predict false");
+console.log(car=='Pagani Huayra');
+
+let country= "UAE";
+let lowerCountry = country.toLowerCase();
+
+console.log("\nIs lowercase Country uae? I Predict true");
+console.log(lowerCountry=='uae');
+
+let num1 = 30;
+let num2 = 20;
+
+console.log("\nIs num1 greater than num2? I predict true.");
+console.log(num1>num2);
+
+console.log("\nIs num1 equal to num2? I predict false.");
+console.log(num1==num2);
+
+let temperature = 10;
+let winter = true;
+
+console.log("\nIs the temperature 10C and winter? I predict true");
+console.log(temperature==10 && winter==true);
+
+console.log("\nIs the temperature 8C or winter? I predict true");
+console.log(temperature==10 || winter==true);
+
+let fruits= ['apple','banana','mango'];
+
+console.log("\nIs apple in the fruits array? I predict true.");
+console.log(fruits.includes('apple'));
+
+console.log("\nIs guava in the fruits array? I predict false.");
+console.log(fruits.includes('guava'));
+
+
+
