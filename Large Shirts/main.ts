d88a9683d851754d32c2ca5c29aade9adf5faf94
AnasSM0 <anasbutt20067@gmail.com>
2024-03-04 11:01:02 +0500

    initial commit

---
+++ Large Shirts/main.ts
@@ -0,0 +1,11 @@
+function makeShirt(size: string="Large",message : string="I Love TypeScript"):void{
+
+    console.log(`You have ordered a ${size} T-shirt with the message ${message}`);
+    
+    }
+    
+makeShirt();
+
+makeShirt("Medium");
+
+makeShirt("Small","TypeScript is amazing");
