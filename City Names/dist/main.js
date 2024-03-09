6d551927362a66856ac07f5ec9becf4f42771666
AnasSM0 <anasbutt20067@gmail.com>
2024-03-09 11:40:29 +0500

    initial commit

---
+++ City Names/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    console.log(`${city},${country}`);
+}
+city_country("Tokyo", "Japan");
+city_country("New York City", "United States");
+city_country("Sydney ", "Australia");
---
+++ City Names/src/main.ts
@@ -0,0 +1,10 @@
+function city_country(city:string,country:string){
+
+console.log(`${city},${country}`);
+
+
+}
+
+city_country("Tokyo","Japan");
+city_country("New York City","United States");
+city_country("Sydney ","Australia");
